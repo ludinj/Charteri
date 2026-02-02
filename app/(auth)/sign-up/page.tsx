352f6e99e7e3607e57db6eb71aeca9b97b66b6f7
ludinj <ljaimes@shokworks.io>
2026-02-02 13:27:09 -0400

    Implemented ui for sign in and sign up pages

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import { CountrySelectField } from "@/components/forms/CountrySelectField";
+import FooterLink from "@/components/forms/FooterLink";
+import InputField from "@/components/forms/InputField";
+import SelectField from "@/components/forms/SelectField";
+import { Button } from "@/components/ui/button";
+import {
+  INVESTMENT_GOALS,
+  PREFERRED_INDUSTRIES,
+  RISK_TOLERANCE_OPTIONS,
+} from "@/lib/constants";
+import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+const SignUpPage = () => {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm<SignUpFormDataI>({
+    defaultValues: {
+      fullName: "",
+      email: "",
+      password: "",
+      country: "US",
+      investmentGoals: "Growth",
+      riskTolerance: "Medium",
+      preferredIndustry: "Technology",
+    },
+    mode: "onBlur",
+  });
+  const onSubmit = async (data: SignUpFormDataI) => {
+    try {
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <h1 className="form-title">Sing Up and Personalize</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+        {/* Inputs */}
+        <InputField
+          name="fullName"
+          label="Full Name"
+          placeholder="John Doe"
+          register={register}
+          error={errors.fullName}
+          validation={{ require: "Full name is required", minLength: 2 }}
+        />
+        <InputField
+          name="email"
+          label="Email"
+          placeholder="john-doe@gmail.com"
+          register={register}
+          error={errors.email}
+          validation={{
+            require: "Email is required",
+            minLength: 2,
+            pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+            message: "Invalid Email",
+          }}
+        />
+        <InputField
+          name="password"
+          label="Password"
+          placeholder="Enter a strong password"
+          type="password"
+          register={register}
+          error={errors.password}
+          validation={{ require: "Password is required", minLength: 8 }}
+        />
+        <CountrySelectField
+          name="country"
+          label="Country"
+          control={control}
+          error={errors.country}
+          required
+        />
+        {/* Selects */}
+        <SelectField
+          name="investmentGoals"
+          label="Investment Goals"
+          placeholder="Select your investment goal"
+          control={control}
+          options={INVESTMENT_GOALS}
+          error={errors.investmentGoals}
+          required
+        />
+        <SelectField
+          name="riskTolerance"
+          label="Risk Tolerance"
+          placeholder="Select your risk level"
+          control={control}
+          options={RISK_TOLERANCE_OPTIONS}
+          error={errors.riskTolerance}
+          required
+        />
+        <SelectField
+          name="preferredIndustry"
+          label="Preferred Industry"
+          placeholder="Select your preferred industry"
+          control={control}
+          options={PREFERRED_INDUSTRIES}
+          error={errors.preferredIndustry}
+          required
+        />
+        <Button
+          type="submit"
+          disabled={isSubmitting}
+          className="yellow-btn w-full mt-5"
+        >
+          {isSubmitting ? "Creating Account" : "Start Your Investing Journey"}
+        </Button>
+        <FooterLink
+          text="Already have an account"
+          linkText="Sign in"
+          href="/sign-in"
+        />
+      </form>
+    </>
+  );
+};
+
+export default SignUpPage;
